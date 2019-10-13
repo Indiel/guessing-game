5e680464ed112364622b888f0855d7ad35cbc394
Indiel <kori.yanagi@gmail.com>
2019-10-13 11:31:59 +0200

    feat: task completed

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min;
+        this.max;
+        this.middle;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.middle = Math.round((this.min + this.max) / 2);
+        return this.middle;
     }
 
     lower() {
-
+        this.max = this.middle;
+        this.guess();
     }
 
     greater() {
-
+        this.min = this.middle;
+        this.guess();
     }
 }
 
